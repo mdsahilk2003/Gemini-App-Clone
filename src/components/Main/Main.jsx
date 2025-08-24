4ea74691ae5580bd0ca44d84e23ccca7d42441c3
Md Sahil <mdsahilk2003@gmail.com>
2025-08-24 13:52:49 +0530

    Renamed files and updated imports

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
-import { Context } from '../../Context/Context'
+import { Context } from '../../context/Context.jsx'
 
 
 const Main = () => {
